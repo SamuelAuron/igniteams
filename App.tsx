4dbd83c829496b0eb1728c2ea908e059eaf69468
Samuel <samueld.r.abreu@gmail.com>
2024-10-10 06:49:43 -0300

    💄 feat: Style the statusbar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
@@ -12,7 +13,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent// Transparent status bar
+      />
       {fontsLoaded ? <Groups /> : <Loading/>}
     </ThemeProvider>
     
