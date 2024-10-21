76dfcd70107399259580460930478319b5b185f5
Samuel <samueld.r.abreu@gmail.com>
2024-10-21 06:57:57 -0300

    "Updated App.tsx to import Players instead of Groups, and added dependencies for react-navigation and react-native-safe-area-context in package.json and package-lock.json."

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Groups } from '@screens/Groups';
+import { Players } from '@screens/Players';
 
 import { Loading } from '@components/Loading'
 
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent// Transparent status bar
       />
-      {fontsLoaded ? <Groups /> : <Loading/>}
+      {fontsLoaded ? <Players /> : <Loading/>}
     </ThemeProvider>
     
   );
