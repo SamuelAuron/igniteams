078fd9b3f3b843b52749daea1f84b389e93ebbf3
Samuel <samueld.r.abreu@gmail.com>
2024-11-03 11:09:23 -0300

    Refactory

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,81 +1,74 @@
-import { useState, useCallback } from 'react';
-import { Alert, FlatList } from 'react-native';
+import { useState, useCallback } from "react";
+import { Alert, FlatList } from "react-native";
 
-import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 
-import { groupsGetAll } from '@storage/group/groupsGetAll';
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
-import { GroupCard } from '@components/GroupCard';
-import { Header } from '@components/Header';
-import { Highlight } from '@components/Highlight';
-import { ListEmpty } from '@components/ListEmpty';
-import { Button } from '@components/Button';
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
-import { Container } from './styles';
-import { Loading } from '@components/Loading';
+import { Container } from "./styles";
+import { Loading } from "@components/Loading";
 
 export function Groups() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [groups, setGroups] = useState<string[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [groups, setGroups] = useState<string[]>([]);
 
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  function handleNewGroup() {
-    navigation.navigate('new');
-  }
+	function handleNewGroup() {
+		navigation.navigate("new");
+	}
 
-  async function fetchGroups() {
-    try {
-      setIsLoading(true);
-      const data = await groupsGetAll();
-      setGroups(data)
-    } catch (error) {
-      Alert.alert('Turmas', 'Não foi possível carregar as turmas');
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-    } 
-  }
+	async function fetchGroups() {
+		try {
+			setIsLoading(true);
+			const data = await groupsGetAll();
+			setGroups(data);
+		} catch (error) {
+			Alert.alert("Turmas", "Não foi possível carregar as turmas");
+			console.log(error);
+		} finally {
+			setIsLoading(false);
+		}
+	}
 
-  function handleOpenGroup(group: string) {
-    console.log('Abrir turma:', group);
-    navigation.navigate('players', { group })
-  }
-  
-  useFocusEffect(useCallback(() => {
-    fetchGroups()
-  },[]))
-  
+	function handleOpenGroup(group: string) {
+		navigation.navigate("players", { group });
+	}
 
-  return (
-    <Container>
-      <Header />
-      <Highlight 
-        title="Turmas"
-        subtitle="jogue com sua turma"
-      />
-      {
-        isLoading ? <Loading /> :
-          <FlatList 
-            data={groups}
-            keyExtractor={item => item}
-            renderItem={({ item }) => (
-              <GroupCard 
-                title={item}
-                onPress={() => handleOpenGroup(item)}
-              />
-            )}
-            contentContainerStyle={groups.length === 0 && { flex: 1 }}
-            ListEmptyComponent={() => (
-              <ListEmpty message="Que tal cadastrar a primeira turma?" />
-            )}
-          />
-      }
+	useFocusEffect(
+		// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+		useCallback(() => {
+			fetchGroups();
+		}, []),
+	);
 
-      <Button 
-        title='Criar nova turma'
-        onPress={handleNewGroup}
-      />
-    </Container>
-  );
+	return (
+		<Container>
+			<Header />
+			<Highlight title="Turmas" subtitle="jogue com sua turma" />
+			{isLoading ? (
+				<Loading />
+			) : (
+				<FlatList
+					data={groups}
+					keyExtractor={(item) => item}
+					renderItem={({ item }) => (
+						<GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+					)}
+					contentContainerStyle={groups.length === 0 && { flex: 1 }}
+					ListEmptyComponent={() => (
+						<ListEmpty message="Que tal cadastrar a primeira turma?" />
+					)}
+				/>
+			)}
+
+			<Button title="Criar nova turma" onPress={handleNewGroup} />
+		</Container>
+	);
 }
